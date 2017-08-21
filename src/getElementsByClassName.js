827d2320dd420bafe5e9081d8ce8fbf731d3002f
arsimr16 <arsimr16@g.holycross.edy>
2017-08-21 12:31:47 -0700

    attempts to get elements with the targeted class name TO DO: get this to work.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, element = document.body
 ) {
-  // your code here
+  //element = element || document.body;
+  const elementsWithClassNameList = [];
+  //console.log(element.classList);
+  if (element.classList.length > 0 && element.classList.contains(className)) {
+    //console.log(element.classList);
+    elementsWithClassNameList.push(element);
+  }
+  if (element.childNodes.length > 0) {
+    element.childNodes.forEach(function(node) {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        elementsWithClassNameList.concat(getElementsByClassName(className, node));
+      }
+    });
+  }
+  return elementsWithClassNameList;
 };
+
+console.log('DEBUG: ' + getElementsByClassName('targetClassName'));
