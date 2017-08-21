c3683f846d63f4beef787d4437c7d6239ddc79d6
arsimr16 <arsimr16@g.holycross.edy>
2017-08-21 15:50:09 -0700

    gets all elements that have the input class name It works now :)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,23 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, element = document.body
+var getElementsByClassName = function(className, element
 ) {
-  //element = element || document.body;
-  const elementsWithClassNameList = [];
-  //console.log(element.classList);
+  element = element || document.body;
+  var elementsWithClassNameList = [];
+
   if (element.classList.length > 0 && element.classList.contains(className)) {
-    //console.log(element.classList);
+
     elementsWithClassNameList.push(element);
   }
   if (element.childNodes.length > 0) {
     element.childNodes.forEach(function(node) {
       if (node.nodeType === Node.ELEMENT_NODE) {
-        elementsWithClassNameList.concat(getElementsByClassName(className, node));
+        elementsWithClassNameList = elementsWithClassNameList.concat(getElementsByClassName(className, node));
       }
     });
   }
+
   return elementsWithClassNameList;
 };
 
-console.log('DEBUG: ' + getElementsByClassName('targetClassName'));
+
